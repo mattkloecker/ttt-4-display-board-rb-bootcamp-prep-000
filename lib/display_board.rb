8ed1879a4ce4f6547cec410bb84e0915f0031140
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-28 13:42:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,7 @@
 # out the current state.
 def display_board(board)
   board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
-  rows = ["#{board[0]}|#{board[1]}|#{board[2]}", "#{board[3]}|#{board[4]}|#{board[5]}", 
-  "#{board[6]}|#{board[7]}|#{board[8]}"]
-  puts "#{row_1}\n-----------\n#{row_2}\n-----------\n#{row_3}"
+  rows = ["#{board[0]}|#{board[1]}|#{board[2]}", "#{board[3]}|#{board[4]}|#{board[5]}", "#{board[6]}|#{board[7]}|#{board[8]}"]
+  puts "#{rows[0]}\n-----------\n#{rows[1]}\n-----------\n#{rows[2]}"
 
 end
