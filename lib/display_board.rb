d0856e0c22a54d3fdc7f369f9451b4fa2e017d9a
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-28 13:40:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(state="   ")
-  cell = "   "
-  row_1 = "#{cell}|#{cell}|#{cell}"
-  row_2 = 
-  row_3 = 
+def display_board(board)
+  board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+  row_1 = "#{board[0]}|#{board[1]}|#{board[2]}"
+  row_2 = "#{board[3]}|#{board[4]}|#{board[5]}"
+  row_3 = "#{board[6]}|#{board[7]}|#{board[8]}"
   puts "#{row_1}\n-----------\n#{row_2}\n-----------\n#{row_3}"
 
 end
